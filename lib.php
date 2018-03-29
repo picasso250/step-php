e96c8d09075cb7cae74e6a194736d67e9ef07a28
wangxiaochi <wangxiaochi@tuhu.cn>
2018-03-29 16:54:26 +0800

    data can be null

--- lib.php
+++ lib.php
@@ -1,6 +1,6 @@
 <?php
 
-function render_with_layout($layout_tpl, $inner_tpl_list, $data)
+function render_with_layout($layout_tpl, $inner_tpl_list, $data = [])
 {
     $data['_inner_tpl_list'] = $inner_tpl_list;
     extract($data);
