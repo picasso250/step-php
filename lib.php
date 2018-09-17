c243c48ba58461b49b88bdcdcfb0ffa5748fb1bd
王霄池(wangxiaochi) <cumt.xiaochi@gmail.com>
2018-09-17 13:39:39 +0800

    Update lib.php

--- lib.php
+++ lib.php
@@ -15,3 +15,28 @@ function render_with_layout($layout_tpl, $inner_tpl_list, $data = [])
     extract($data);
     include $layout_tpl;
 }
+function sv($name,$value=null){
+    static $lazy;
+    static $pool;
+    if ($value=== null){
+        // get
+        if(isset($pool[$name]))return $pool[$name];
+        if(isset($lazy[$name]))return $pool[$name]=($lazy[$name])();
+        return null;
+    } else {
+        // set
+        if (is_callable($value)) $lazy[$name]=$value;
+        else $pool[$name]=$value;
+    }
+}
+function dot_env($root=""){
+    if (defined("ROOT")&&$root==="") $root=ROOT;
+    $file="$root/.env";
+    if(!file_exists($file))die("no .env file");
+    $vars=parse_ini_file($file);
+    foreach($vars as $k=>$v){
+        $_ENV[$k]=$v;
+    }
+}
+    
+          
