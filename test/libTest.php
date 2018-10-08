3a781980f280c6767f12850f5bb819ec19f56c17
picasso250 <cumt.xiaochi@gmail.com>
2018-10-08 22:19:42 +0800

    test Res

--- test/libTest.php
+++ test/libTest.php
@@ -32,4 +32,13 @@ final class libTest extends TestCase
         unlink($file);
         rmdir($dir);
     }
+    public function testCanRender()
+    {
+        $a = 42;
+        Res::$layout_tpl = __DIR__.'/layout.phtml';
+        ob_start();
+        Res::renderWithLayout(['content'=>__DIR__.'/inner.phtml'], compact('a'));
+        $text = ob_get_clean();
+        $this->assertEquals("a42b", $text);
+    }
 }
